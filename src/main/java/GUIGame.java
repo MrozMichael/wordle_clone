7e0a1d616ba3b43c738fda65012c024af02513b6
Michael Mroz <michaelmroz01111@gmail.com>
2023-12-28 16:03:25 -0400

    Add start screen to UI to dynamically create n by n grid

---
+++ src/main/java/GUIGame.java
@@ -0,0 +1,7 @@
+//Implement Wordle Game logic for Javafx app Wordle_UI
+public class GUIGame {
+
+    //on start, prompt user for 5 6 or 7 letter word (buttons)
+    //on button press, set up the n/n grid.
+
+}
--- src/main/java/Wordle_UI.java
+++ src/main/java/Wordle_UI.java
@@ -1,44 +1,79 @@
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import  javafx.scene.control.Label;
-import java.awt.*;
 
 public class Wordle_UI extends Application {
     private double sceneWidth = 1024;
     private double sceneHeight = 768;
     private Node[][] board;
+
+    private int wordLen;
+
     public static void main(String[] args) {
-        //GUIGame.main(args);
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-    BorderPane root = new BorderPane();
-    int n = 5; //replace with user input letters/lives
-    this.board = new Node[n][n];
+    public void start(Stage stage) {
 
-    HBox top = createTop();
-    GridPane grid = createGrid(n);
-    BorderPane.setMargin(grid, new Insets(5, 5, 0, sceneWidth/4));
-    root.setTop(top);
-    root.setCenter(grid);
-    Scene viewport = new Scene(root);
-    primaryStage.setScene(viewport);
-    primaryStage.setTitle("Wordle Clone");
-    primaryStage.show();
+        BorderPane startRoot = new BorderPane();
+        VBox elements = new VBox();
+        elements.setStyle("-fx-border-color: black");
+        Label welcomeText = new Label("Select word size");
+        configureLabel(welcomeText);
+        HBox buttons = new HBox();
+
+        Button five = new Button("5-Letter Word");
+        Button six = new Button("6-Letter Word");
+        Button seven = new Button("7-Letter Word");
+
+        buttons.getChildren().addAll(five, six, seven);
+        configureButtons(buttons);
+        elements.getChildren().addAll(welcomeText, buttons);
+        startRoot.setCenter(elements);
+        Scene startScene = new Scene(startRoot);
+        stage.setScene(startScene);
+        configureStartWindow(stage);
+        stage.setTitle("Wordle Clone");
+        stage.show();
+        seven.setOnAction((event) -> {
+            wordLen = 7;
+            changeScene(stage);
+        });
+        six.setOnAction((event) -> {
+            wordLen = 6;
+            changeScene(stage);
+        });
+        five.setOnAction((event) -> {
+            wordLen = 5;
+            changeScene(stage);
+        });
+
+    }
+
+    public void configureStartWindow(Stage stage){
+        stage.setMinHeight(250);
+        stage.setMinWidth(500);
+        stage.setMaxHeight(250);
+        stage.setMaxWidth(500);
+    }
+
+    public void configureGameWindow(Stage stage){
+        stage.setMinWidth(1200);
+        stage.setMinHeight(800);
+        stage.setMaxHeight(Double.MAX_VALUE);
+        stage.setMaxWidth(Double.MAX_VALUE);
     }
 
     public HBox createTop(){
         HBox top = new HBox();
         Label title = new Label("Wordle-Clone");
         top.getChildren().add(title);
-        HBox.setMargin(title, new Insets(5, 5, 5,  sceneWidth/2));
+        HBox.setMargin(title, new Insets(5, 5, 5,  sceneWidth/4));
         top.setPadding(new Insets(20, 20, 20, 20));
         top.setSpacing(10);
         top.setStyle("-fx-font-size: 50;");
@@ -46,8 +81,8 @@ public class Wordle_UI extends Application {
     }
     public GridPane createGrid(int n){
         GridPane grid = new GridPane();
-        double gridWidth = sceneWidth / n;
-        double gridHeight = sceneHeight/ n;
+        double gridWidth = 80;
+        double gridHeight = 80;
         for (int i = 0; i < n; i++){
             for (int k = 0; k < n; k++) {
                 Node node = new Node("", i *gridWidth, k * gridHeight, gridWidth, gridHeight);
@@ -56,5 +91,29 @@ public class Wordle_UI extends Application {
             }
         }
         return grid;
+    }
+    public void configureLabel(Label label){
+        label.setMinHeight(50);
+        label.setMinWidth(80);
+        label.setStyle("-fx-font-size: 40;");
+        VBox.setMargin(label, new Insets(50, 0, 0, 100));
+    }
+
+    public void configureButtons(HBox buttons){
+        VBox.setMargin(buttons, new Insets(50, 0, 0, 100) );
+    }
+
+    public void changeScene(Stage stage){
+        BorderPane mainRoot = new BorderPane();
+        int n = wordLen; //replace with user input letters/lives
+        this.board = new Node[n][n];
+        HBox top = createTop();
+        GridPane grid = createGrid(n);
+        BorderPane.setMargin(grid, new Insets(5, 5, 0, sceneWidth/5));
+        mainRoot.setTop(top);
+        mainRoot.setCenter(grid);
+        Scene gameScene = new Scene(mainRoot);
+        configureGameWindow(stage);
+        stage.setScene(gameScene);
     }
 }
